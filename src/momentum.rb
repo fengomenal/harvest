0a05b28748639fd660904f1ee92845c0dff5bc91
fengomenal <ubuntu@ip-172-31-25-145.ec2.internal>
2022-02-22 01:18:57 +0000

    wip files

--- src/momentum.rb
+++ src/momentum.rb
@@ -19,76 +19,29 @@ scaffold_dates.each do |date|
   selected_dates << date
 end
 
-selected_dates = selected_dates[0..10]
+selected_dates = selected_dates[0..100]
 
-rebalanced_portfolio = {}
-
-
-prev_period_data = client.query("SELECT ticker,adj_close FROM historical WHERE rec_date='#{selected_dates[0]}'").map { |i| i }
-period_data = nil
-selected_dates[1..-1].each do |date|
-  period_data = client.query("SELECT ticker,adj_close FROM historical WHERE rec_date='#{date}'").map { |i| i }
-  rebalanced_portfolio.each do |ticker, |
-    
+portfolio = {}
+value = 1
+selected_dates.each do |date|
+  period_data = {}
+  rows = client.query("SELECT ticker,adj_close FROM historical WHERE rec_date='#{date}'").map { |i| i }
+  rows.each { |row| period_data[row['ticker']] = row['adj_close'] }
+  unless portfolio.empty?
+    value = value * Harvest::Util.calculate_new_value(portfolio, period_data) 
   end
-  prev_period_data = period_data
-end
-fail
-
-tickers = ["NRP", "LGO", "ARMP", "ACTG", "MTA", "AAPL", "A"]
-tickers.shuffle.each do |ticker|
-  data = client.query("SELECT rec_date,adj_close FROM historical WHERE ticker='#{ticker}'").sort { |i| i['rec_date'] }.reverse
-  next if data.any? { |i| i['adj_close'].zero? }
-  next unless data.first['rec_date'] <= start_cutoff
-  historical[ticker] = data.select { |val| val['rec_date'] >= start_cutoff }
-  count += 1
-  break if count ==  max_tickers
+  puts value
+  portfolio = {}
+  non_zero = period_data.keys.select { |ticker| !period_data[ticker].zero? }
+  allocation = 1.0 / non_zero.size
+  non_zero.each { |ticker| portfolio[ticker] = { allocation: allocation, price: period_data[ticker] } }
 end
 
-monthly_data = {}
-monthly_maxes = {}
-historical.each do |ticker, data|
-  monthly_partition = Util.partition_via(data, 'day', 'rec_date')
-  monthly_max = monthly_partition.map { |m| m.max_by { |d| d['adj_close'] }['adj_close'] }
-  monthly_maxes[ticker] = monthly_max
-  monthly_partition.each_with_index do |month, index|
-    start_date = month.first['rec_date']
-    key_string = "#{start_date.year}-#{"%02d" % start_date.month}"
-    monthly_data[key_string] ||= {}
-    monthly_data[key_string][ticker] = month.first['adj_close']
-  end
-end
-
-months = monthly_data.keys.sort
-tickers = historical.keys
-performance_range = ['2016-02', '2016-05']
 # momentum portfolio
-momentum_portfolio = {}
-months[11..-1].each_with_index do |month, index|
-  position = {}
-  highest_momentum = monthly_data[month].keys.sort_by { |ticker| monthly_data[month][ticker] / monthly_maxes[ticker][(index)..(index + 10)].max }.reverse[0..4]
-  momentum_portfolio[month] = highest_momentum.map { |ticker| [ticker, 1.0 / 5] }.to_h
-end
-momentum_performance = Util.calculate_performance(momentum_portfolio, monthly_data)
-
-allocation_per = 1.0 / max_tickers
-
-# rebalanced market portfolio
-rebalanced_portfolio = {}
-months[11..-1].each do |month|
-  position = {}
-  tickers.each { |ticker| position[ticker] = allocation_per }
-  rebalanced_portfolio[month] = position
-end
-rebalanced_performance = Util.calculate_performance(rebalanced_portfolio, monthly_data)
-
-# buy and hold market portfolio
-buy_and_hold_portfolio = {}
-buy_and_hold_portfolio[months[11]] = tickers.map { |ticker| [ticker, allocation_per] }.to_h
-buy_and_hold_portfolio[months.last] = tickers.map { |ticker| [ticker, allocation_per] }.to_h
-buy_and_hold_performance = Util.calculate_performance(buy_and_hold_portfolio, monthly_data)
-
-STDOUT.puts "Performance from #{months[11]} to #{months.last}:"
-STDOUT.puts "\tMomentum: #{momentum_performance.values.last - 1}"
-STDOUT.puts "\tRebalanced market: #{rebalanced_performance.values.last - 1}"
-STDOUT.puts "\tBuy and hold: #{buy_and_hold_performance.values.last - 1}"
+#momentum_portfolio = {}
+#months[11..-1].each_with_index do |month, index|
+#  position = {}
+#  highest_momentum = monthly_data[month].keys.sort_by { |ticker| monthly_data[month][ticker] / monthly_maxes[ticker][(index)..(index + 10)].max }.reverse[0..4]
+#  momentum_portfolio[month] = highest_momentum.map { |ticker| [ticker, 1.0 / 5] }.to_h
+#end
+#momentum_performance = Util.calculate_performance(momentum_portfolio, monthly_data)
--- src/util.rb
+++ src/util.rb
@@ -1,54 +1,43 @@
-module Util
-  module_function
+module Harvest
+  module Util
+    module_function
 
-  def partition_to_weeks(data, date_key)
-    res = []
-    res << [data.first]
-    last_wday = data.first[date_key].wday
-    data[1..-1].each do |day|
-      if day[date_key].wday <= last_wday
-        res << [day]
-      else
-        res.last << day
+    def partition_to_weeks(data, date_key)
+      res = []
+      res << [data.first]
+      last_wday = data.first[date_key].wday
+      data[1..-1].each do |day|
+        if day[date_key].wday <= last_wday
+          res << [day]
+        else
+          res.last << day
+        end
+        last_wday = day[date_key].wday
       end
-      last_wday = day[date_key].wday
+      res
     end
-    res
-  end
 
-  def partition_via(data, date_attr, date_key)
-    res = []
-    res << [data.first]
-    last_key = data.first[date_key].send(date_attr)
-    data[1..-1].each do |day|
-      if day[date_key].send(date_attr) <= last_key
-        res << [day]
-      else
-        res.last << day
+    def partition_via(data, date_attr, date_key)
+      res = []
+      res << [data.first]
+      last_key = data.first[date_key].send(date_attr)
+      data[1..-1].each do |day|
+        if day[date_key].send(date_attr) <= last_key
+          res << [day]
+        else
+          res.last << day
+        end
+        last_key = day[date_key].send(date_attr)
       end
-      last_key = day[date_key].send(date_attr)
+      res
     end
-    res
-  end
 
-  def calculate_new_value(position, price_start, price_end)
-    value = 0
-    position.each do |ticker, allocation|
-      shares = allocation / price_start[ticker]
-      value += shares * price_end[ticker]
+    def calculate_new_value(portfolio, period_data)
+      value = 1
+      portfolio.each do |ticker, data|
+        value += data[:allocation] * ((period_data[ticker]|| 0) - data[:price]) / data[:price]
+      end
+      value
     end
-    value
-  end
-
-  def calculate_performance(positions, price_history)
-    value = 1.0
-    performance = {}
-    dates = positions.keys.sort
-    dates[1..-1].each_with_index do |date, index|
-      start_date = dates[index]
-      value = value * calculate_new_value(positions[start_date], price_history[start_date], price_history[date])
-      performance[start_date] = value
-    end
-    performance
   end
 end
